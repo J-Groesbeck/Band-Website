421ed2eaaaa00a0e975391f869364230102d86fb
J-Groesbeck <jacob.groesbeck@icloud.com>
2025-02-23 21:56:49 -0500

    Version 1.0 Completed and ready to be uploaded - Finished Fine Styling || Commented out FAQ as we didn't get info in time || Added tons of Comments (thanks chatGPT) || Added alt tags to all images || Changed twitter link in navbar

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-const platinumMembers = ["Sara Axelrod-Malagold", "Kunjan Bhatt", "Swati Dhage", "Alicia Caputo Smith", "Wei Ou", "Karen Hao", "Alison and Thomas Tsim", "Peggy Sheehan", "Paula Groesbeck", "Nandini Thomas", "Katherine Trezza", "Kay Haeusser", "Lisa Gianna", "Jessica Ho", "Niranjan Mulay / Madhu Mahajan", "Marine Preap", "Rita Shteynberg", "Ushma Patel", "Anna Bobrowska" ]
+// Dynamically generates boosters members in boosters.html accordian
+
+const platinumMembers = ["Sara Axelrod-Malagold", "Kunjan Bhatt", "Swati Dhage", "Alicia Caputo Smith", "Wei Ou", "Karen Hao", "Alison and Thomas Tsim", "Peggy Sheehan", "Paula Groesbeck", "Nandini Thomas", "Katherine Trezza", "Kay Haeusser", "Lisa Gianna", "Jessica Ho", "Niranjan Mulay / Madhu Mahajan", "Marine Preap", "Rita Shteynberg", "Ushma Patel", "Anna Bobrowska"]
 
 platinumMembers.forEach(member => {
     $('#platMembers').append(`<li>${member}</li>`);
 });
 
-const goldMembers = ["Robert Cameron", "Yanina Reznikov",  ]
+const goldMembers = ["Robert Cameron", "Yanina Reznikov",]
 
 goldMembers.forEach(member => {
     $('#golMembers').append(`<li>${member}</li>`);
 });
 
-const silverMembers = ["Michelle Wolf", "Betty Chang", "Melanie Chazanoff", "Chutchauan T", "Monika Kokosza", "Bheemesh Dwarampuri", "Lauren Faulkner", "Archana Kannughatta" ]
+const silverMembers = ["Michelle Wolf", "Betty Chang", "Melanie Chazanoff", "Chutchauan T", "Monika Kokosza", "Bheemesh Dwarampuri", "Lauren Faulkner", "Archana Kannughatta"]
 
 silverMembers.forEach(member => {
     $('#silMembers').append(`<li>${member}</li>`);
 });
 
-const bronzeMembers = ["Lisa Cartelli", "Jamie Sobolewski", ]
+const bronzeMembers = ["Lisa Cartelli", "Jamie Sobolewski",]
 
 bronzeMembers.forEach(member => {
     $('#broMembers').append(`<li>${member}</li>`);
 });
 
+
+//Back to top button
 // Get the button:
 let topbutton = document.getElementById("topBtn");
 
